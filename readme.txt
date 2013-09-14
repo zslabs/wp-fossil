d56d748001aab318bcb9e2c39b38b481a4197a10
Zach Schnackel <zachschnackel@gmail.com>
2013-09-14 11:31:10 -0400

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,33 @@
+=== Plugin Name ===
+Contributors: zslabs
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=EEMPDX7SN4RFW
+Tags: ie, ie8, media queries, css3
+Requires at least: 3.5
+Tested up to: 3.6
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Provides support for media queries and emulating CSS3 pseudo-classes and attribute selectors in Internet Explorer 6-8.
+
+== Description ==
+
+A handy utility plugin that provides support for media queries and emulating CSS3 pseudo-classes and attribute selectors in Internet Explorer 6-8. Uses [Selectivizr](https://github.com/keithclark/selectivizr) and [Respond.js](https://github.com/scottjehl/Respond).
+
+_Uses conditional statements to only load these libraries in IE8 or older._
+
+== Installation ==
+
+1. Upload the `wp-fossil` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= Can you add `feature-x`? =
+
+Sure! I'm always open to knew ideas. Just create a new [issue](https://github.com/zslabs/wp-fossil/issues) and I'll take a gander.
+
+== Changelog ==
+
+= 0.1 =
+* Initial release
---
+++ wp-fossil.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+Plugin Name: WP Fossil
+Plugin URI: http://zslabs.com
+Description: Provides support for media queries and emulating CSS3 pseudo-classes and attribute selectors in Internet Explorer 6-8.
+Version: 0.1
+Author: Zach Schnackel
+Author URI: http://zslabs.com
+*/
+
+/**
+ * Copyright (c) 2013 Zach Schnackel. All rights reserved.
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * **********************************************************************
+ */
+
+/**
+ * Load scripts
+ * Need to do this inline until wp_add_script_data() is supported (as twentytwelve does)
+ * @return void
+ */
+function wpf_load_scripts() { ?>
+	<!--[if lt IE 9]>
+		<script src="<?php echo plugin_dir_url(__FILE__); ?>assets/js/scripts.min.js" type="text/javascript"></script>
+	<![endif]-->
+<?php }
+add_action( 'wp_head', 'wpf_load_scripts', 8 );
